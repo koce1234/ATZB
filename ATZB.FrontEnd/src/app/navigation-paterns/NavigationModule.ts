a96f21266abb4a4be377367bcedf14b33e74a474
Radko <radko@wetrack.com>
2019-08-09 22:16:26 +0300

    merge

--- ATZB.FrontEnd/src/app/navigation-paterns/NavigationModule.ts
+++ ATZB.FrontEnd/src/app/navigation-paterns/NavigationModule.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-
 import { MainNavNotLogedInComponent } from './main-nav-not-loged-in/main-nav-not-loged-in.component';
 import { MainNavComponent } from './main-nav-loged-in/main-nav.component';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../MaterialModule';
+import { Routes, RouterModule } from '@angular/router';
+import { UserLoginComponent } from '../user-pages/user-login/user-login.component';
+import { UserRegisterAsClientComponent } from '../user-pages/user-register-as-client/user-register-as-client.component';
+import { UserRegisterAsPerformerComponent } from '../user-pages/user-register-as-performer/user-register-as-performer.component';
+import { HomePageComponent } from '../sheard/home-page/home-page.component';
+import { PageNotFoundComponent } from '../sheard/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent, pathMatch: 'full' },
+  { path: 'registerAsClient', component: UserRegisterAsClientComponent },
+  { path: 'registeAsPerformer', component: UserRegisterAsPerformerComponent },
+  { path: 'login', component: UserLoginComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
     declarations: [
@@ -13,16 +26,16 @@ import { MaterialModule } from '../MaterialModule';
         MainNavNotLogedInComponent,
     ],
     imports: [
-        AppRoutingModule,
         CommonModule,
         MaterialModule,
+        RouterModule.forRoot(routes),
     ],
     exports: [
-        AppRoutingModule,
         CommonModule,
         MainNavComponent,
         MainNavNotLogedInComponent,
         MaterialModule,
+        RouterModule,
     ]
 })
 export class NavigationModule{}
